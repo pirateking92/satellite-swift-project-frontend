e95d4369c5f95bd0c53de03b1911f6f8dc1a177e
Oli Kelly <93444456+olikelly00@users.noreply.github.com>
2024-04-16 13:00:44 +0100

    added gitignore file

---
+++ MobileAcebook/Preview Content/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  MobileAcebook
+//
+//  Created by Oli Kelly on 16/04/2024.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
